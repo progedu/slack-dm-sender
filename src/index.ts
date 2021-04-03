a9c1f66d70e59aec02a26246040053f54e99b5b0
Soichiro Yoshimura <soichiro_yoshimura@nnn.ac.jp>
2021-04-03 13:03:33 +0900

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+'use strict';
+import * as fs from 'fs';
+// WebAPI Method について → https://api.slack.com/methods
+// Slack SDK WebAPI について → https://slack.dev/node-slack-sdk/web-api
+import { WebClient, WebAPICallResult } from '@slack/web-api';
+
+// メイン処理
+(async () => {
+    let counter = 0;
+
+
+    console.log(`[INFO] 全 ${counter} 件の処理を終えました。`);
+})();
